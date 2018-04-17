36a320fb518baffb1a70646b5064a8d9987233a7
John <johnngo@Johns-MacBook-Pro.local>
2018-04-17 15:29:02 -0700

    react friends

---
+++ react-friends/src/PeopleList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Card, CardTitle, CardSubtitle, CardBody } from 'reactstrap';
+import FriendList from './FriendList';
+import './PeopleList.css';
+
+const PeopleList = props => {
+    return (
+        <div>
+            {props.people.map(person => (
+                <Card>
+                          <CardBody key={person.id}>
+                            <CardTitle>{`${person.first_name} ${person.last_name}`}</CardTitle>
+                            <CardSubtitle>{person.email}</CardSubtitle>
+                            <FriendList friends={person.friends} />
+                         </CardBody>
+                        </Card>
+            ))}; 
+           
+        </div>
+    );
+};
+
+export default PeopleList;
